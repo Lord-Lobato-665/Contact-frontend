965dd00c96486c88c8e39c42d5aa0d965ec76b40
Gael Lobato <gaelsanchez509@gmail.com>
2025-07-02 19:47:07 -0500

    Listo

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import { install } from 'vue3-recaptcha-v2';
 createApp(App)
   .use(router)
   .use(install, {
-    sitekey: '6Lf5yW4rAAAAAJWRpMyyGa-__ern80Iiu9EeKcyN',
+    sitekey: '6LfGkHUrAAAAACCjzjVV3F0H7VG7A6Bzz_RL-Dbi',
   })
   .mount('#app');
